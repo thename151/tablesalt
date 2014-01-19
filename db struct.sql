6096466e3a719e6329ee2d7cabc4e2ff3117542d
thename151 <thename151@yahoo.com>
2014-01-19 06:14:43 +0000

    added a table and a view, made some edits Signed-off-by: thename151 <thename151@yahoo.com>

--- db struct.sql
+++ db struct.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2014 at 06:13 AM
+-- Generation Time: Jan 19, 2014 at 05:24 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `login1` (
   `loginName` varchar(25) NOT NULL,
   `loginTime` datetime NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=457 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=475 ;
 
 -- --------------------------------------------------------
 
@@ -53,31 +53,17 @@ CREATE TABLE IF NOT EXISTS `messages1` (
 -- --------------------------------------------------------
 
 --
--- Stand-in structure for view `priceview1`
+-- Table structure for table `priceview1`
 --
-CREATE TABLE IF NOT EXISTS `priceview1` (
-`uniqueX` int(11)
-,`creator1` varchar(25)
-,`product1` varchar(40)
-,`creator2` varchar(25)
-,`product2` varchar(40)
-,`price1` decimal(6,3)
-,`ct` bigint(21)
-);
+-- in use(#1356 - View 'web7.priceview1' references invalid table(s) or column(s) or function(s) or definer/invoker of view lack rights to use them)
+
 -- --------------------------------------------------------
 
 --
--- Stand-in structure for view `priceview5`
+-- Table structure for table `priceview5`
 --
-CREATE TABLE IF NOT EXISTS `priceview5` (
-`uniqueX` int(11)
-,`creator1` varchar(25)
-,`product1` varchar(40)
-,`creator2` varchar(25)
-,`product2` varchar(40)
-,`price2` decimal(6,3)
-,`ct` bigint(21)
-);
+-- in use(#1356 - View 'web7.priceview5' references invalid table(s) or column(s) or function(s) or definer/invoker of view lack rights to use them)
+
 -- --------------------------------------------------------
 
 --
@@ -92,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `products1` (
   `status1` varchar(25) NOT NULL DEFAULT 'okay',
   `dateTime` datetime NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
 
 -- --------------------------------------------------------
 
@@ -191,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `querylog` (
   `theQuery` varchar(250) NOT NULL,
   `dateTime` datetime NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT AUTO_INCREMENT=185 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT AUTO_INCREMENT=2392 ;
 
 -- --------------------------------------------------------
 
@@ -203,7 +189,6 @@ CREATE TABLE IF NOT EXISTS `sales3` (
   `uniqueX` int(11) NOT NULL AUTO_INCREMENT,
   `amount1` decimal(6,3) NOT NULL,
   `type1` varchar(25) NOT NULL,
-  `stock` decimal(9,6) NOT NULL,
   `creator1` varchar(25) NOT NULL,
   `product1` varchar(40) NOT NULL,
   `creator2` varchar(25) NOT NULL,
@@ -212,10 +197,41 @@ CREATE TABLE IF NOT EXISTS `sales3` (
   `price2` decimal(6,3) NOT NULL,
   `user` varchar(25) NOT NULL,
   `dateTime` datetime NOT NULL,
-  `dateTime2` datetime NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=818 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1052 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesactive`
+--
+
+CREATE TABLE IF NOT EXISTS `salesactive` (
+  `uniqueX` int(11) NOT NULL AUTO_INCREMENT,
+  `saleId` int(11) NOT NULL,
+  `stock` decimal(6,3) NOT NULL,
+  PRIMARY KEY (`uniqueX`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=231 ;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `salesactive2`
+--
+CREATE TABLE IF NOT EXISTS `salesactive2` (
+`uniqueX` int(11)
+,`saleId` int(11)
+,`stock` decimal(6,3)
+,`amount1` decimal(6,3)
+,`type1` varchar(25)
+,`creator1` varchar(25)
+,`product1` varchar(40)
+,`creator2` varchar(25)
+,`product2` varchar(40)
+,`price1` decimal(6,3)
+,`price2` decimal(6,3)
+,`user` varchar(25)
+);
 -- --------------------------------------------------------
 
 --
@@ -229,7 +245,7 @@ CREATE TABLE IF NOT EXISTS `scores1` (
   `product` varchar(40) NOT NULL,
   `amount` decimal(6,3) NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=117 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=127 ;
 
 -- --------------------------------------------------------
 
@@ -247,7 +263,7 @@ CREATE TABLE IF NOT EXISTS `sendreclog1` (
   `sendsort` varchar(25) NOT NULL,
   `dateLog` datetime NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1478 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1729 ;
 
 -- --------------------------------------------------------
 
@@ -258,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `sendreclog1` (
 CREATE TABLE IF NOT EXISTS `tradelog` (
   `uniqueX` int(11) NOT NULL AUTO_INCREMENT,
   `odduser` varchar(25) NOT NULL,
-  `wouldsend` decimal(10,9) NOT NULL,
+  `wouldsend` decimal(7,6) NOT NULL,
   `doessend` decimal(4,3) NOT NULL,
   `trade1ux` int(11) NOT NULL,
   `user1` varchar(25) NOT NULL,
@@ -274,7 +290,7 @@ CREATE TABLE IF NOT EXISTS `tradelog` (
   `price1` decimal(9,6) NOT NULL,
   `dateTime` datetime NOT NULL,
   PRIMARY KEY (`uniqueX`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=226 ;
 
 -- --------------------------------------------------------
 
@@ -293,29 +309,11 @@ CREATE TABLE IF NOT EXISTS `users1` (
 -- --------------------------------------------------------
 
 --
--- Structure for view `priceview1`
---
-DROP TABLE IF EXISTS `priceview1`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `priceview1` AS select `sales3`.`uniqueX` AS `uniqueX`,`sales3`.`creator1` AS `creator1`,`sales3`.`product1` AS `product1`,`sales3`.`creator2` AS `creator2`,`sales3`.`product2` AS `product2`,`sales3`.`price1` AS `price1`,count(0) AS `ct` from `sales3` where `sales3`.`price1` in (select min(`sales3`.`price1`) from `sales3` where (`sales3`.`stock` > 0) group by `sales3`.`creator1`,`sales3`.`product1`,`sales3`.`creator2`,`sales3`.`product2`) group by `sales3`.`creator1`,`sales3`.`product1`,`sales3`.`creator2`,`sales3`.`product2` order by `sales3`.`price1` limit 0,30;
-
--- --------------------------------------------------------
-
---
--- Structure for view `priceview5`
---
-DROP TABLE IF EXISTS `priceview5`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `priceview5` AS select `sales3`.`uniqueX` AS `uniqueX`,`sales3`.`creator1` AS `creator1`,`sales3`.`product1` AS `product1`,`sales3`.`creator2` AS `creator2`,`sales3`.`product2` AS `product2`,`sales3`.`price2` AS `price2`,count(0) AS `ct` from `sales3` where `sales3`.`price2` in (select max(`sales3`.`price2`) from `sales3` where (`sales3`.`stock` > 0) group by `sales3`.`creator2`,`sales3`.`product2`,`sales3`.`creator1`,`sales3`.`product1`) group by `sales3`.`creator2`,`sales3`.`product2`,`sales3`.`creator1`,`sales3`.`product1` order by `sales3`.`uniqueX` limit 0,30;
-
--- --------------------------------------------------------
-
---
 -- Structure for view `pv01`
 --
 DROP TABLE IF EXISTS `pv01`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv01` AS select `sales3`.`uniqueX` AS `uniqueX`,`sales3`.`creator1` AS `creator1`,`sales3`.`product1` AS `product1`,`sales3`.`creator2` AS `creator2`,`sales3`.`product2` AS `product2`,`sales3`.`price1` AS `price1`,count(0) AS `ct` from `sales3` where `sales3`.`price1` in (select min(`sales3`.`price1`) from `sales3` where (`sales3`.`stock` > 0) group by `sales3`.`creator1`,`sales3`.`product1`,`sales3`.`creator2`,`sales3`.`product2`) group by `sales3`.`creator1`,`sales3`.`product1`,`sales3`.`creator2`,`sales3`.`product2` order by `sales3`.`price1` limit 0,30;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv01` AS select `salesactive2`.`uniqueX` AS `uniqueX`,`salesactive2`.`creator1` AS `creator1`,`salesactive2`.`product1` AS `product1`,`salesactive2`.`creator2` AS `creator2`,`salesactive2`.`product2` AS `product2`,`salesactive2`.`price1` AS `price1`,count(0) AS `ct` from `salesactive2` where `salesactive2`.`price1` in (select min(`salesactive2`.`price1`) from `salesactive2` where (`salesactive2`.`stock` > 0) group by `salesactive2`.`creator1`,`salesactive2`.`product1`,`salesactive2`.`creator2`,`salesactive2`.`product2`) group by `salesactive2`.`creator1`,`salesactive2`.`product1`,`salesactive2`.`creator2`,`salesactive2`.`product2` order by `salesactive2`.`price1` limit 0,30;
 
 -- --------------------------------------------------------
 
@@ -324,7 +322,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `pv02`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv02` AS select `sales3`.`uniqueX` AS `uniqueX`,`sales3`.`creator1` AS `creator1`,`sales3`.`product1` AS `product1`,`sales3`.`creator2` AS `creator2`,`sales3`.`product2` AS `product2`,`sales3`.`price2` AS `price2`,count(0) AS `ct` from `sales3` where `sales3`.`price2` in (select max(`sales3`.`price2`) from `sales3` where (`sales3`.`stock` > 0) group by `sales3`.`creator2`,`sales3`.`product2`,`sales3`.`creator1`,`sales3`.`product1`) group by `sales3`.`creator2`,`sales3`.`product2`,`sales3`.`creator1`,`sales3`.`product1` order by `sales3`.`uniqueX` limit 0,30;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv02` AS select `salesactive2`.`uniqueX` AS `uniqueX`,`salesactive2`.`creator1` AS `creator1`,`salesactive2`.`product1` AS `product1`,`salesactive2`.`creator2` AS `creator2`,`salesactive2`.`product2` AS `product2`,`salesactive2`.`price2` AS `price2`,count(0) AS `ct` from `salesactive2` where `salesactive2`.`price2` in (select max(`salesactive2`.`price2`) from `salesactive2` where (`salesactive2`.`stock` > 0) group by `salesactive2`.`creator2`,`salesactive2`.`product2`,`salesactive2`.`creator1`,`salesactive2`.`product1`) group by `salesactive2`.`creator2`,`salesactive2`.`product2`,`salesactive2`.`creator1`,`salesactive2`.`product1` order by `salesactive2`.`uniqueX` limit 0,30;
 
 -- --------------------------------------------------------
 
@@ -333,7 +331,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `pv1`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv1` AS select `sales3`.`creator1` AS `pvc1`,`sales3`.`product1` AS `pvp1`,`sales3`.`creator2` AS `pvc2`,`sales3`.`product2` AS `pvp2` from `sales3` limit 0,30;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv1` AS select `salesactive2`.`creator1` AS `pvc1`,`salesactive2`.`product1` AS `pvp1`,`salesactive2`.`creator2` AS `pvc2`,`salesactive2`.`product2` AS `pvp2` from `salesactive2` limit 0,30;
 
 -- --------------------------------------------------------
 
@@ -342,7 +340,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `pv2`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv2` AS select `sales3`.`creator2` AS `pvc1`,`sales3`.`product2` AS `pvp1`,`sales3`.`creator1` AS `pvc2`,`sales3`.`product1` AS `pvp2` from `sales3` limit 0,30;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv2` AS select `salesactive2`.`creator2` AS `pvc1`,`salesactive2`.`product2` AS `pvp1`,`salesactive2`.`creator1` AS `pvc2`,`salesactive2`.`product1` AS `pvp2` from `salesactive2` limit 0,30;
 
 -- --------------------------------------------------------
 
@@ -370,6 +368,15 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 DROP TABLE IF EXISTS `pv5`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pv5` AS select `pv4`.`pvc2` AS `pvc1`,`pv4`.`pvp2` AS `pvp1`,`pv4`.`pvc1` AS `pvc2`,`pv4`.`pvp1` AS `pvp2`,`pv02`.`price2` AS `price1`,`pv4`.`price1` AS `price2` from (`pv4` left join `pv02` on(((`pv4`.`pvc1` = `pv02`.`creator1`) and (`pv4`.`pvp1` = `pv02`.`product1`) and (`pv4`.`pvc2` = `pv02`.`creator2`) and (`pv4`.`pvp2` = `pv02`.`product2`)))) where ((`pv4`.`price1` > 0) or (`pv02`.`price2` > 0)) order by `pv02`.`price2`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `salesactive2`
+--
+DROP TABLE IF EXISTS `salesactive2`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `salesactive2` AS select `salesactive`.`uniqueX` AS `uniqueX`,`salesactive`.`saleId` AS `saleId`,`salesactive`.`stock` AS `stock`,`sales3`.`amount1` AS `amount1`,`sales3`.`type1` AS `type1`,`sales3`.`creator1` AS `creator1`,`sales3`.`product1` AS `product1`,`sales3`.`creator2` AS `creator2`,`sales3`.`product2` AS `product2`,`sales3`.`price1` AS `price1`,`sales3`.`price2` AS `price2`,`sales3`.`user` AS `user` from (`salesactive` left join `sales3` on((`sales3`.`uniqueX` = `salesactive`.`saleId`))) where (`sales3`.`uniqueX` > 0) limit 0,30;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
